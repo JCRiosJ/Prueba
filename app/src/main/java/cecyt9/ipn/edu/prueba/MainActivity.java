bc236c9f82fa557b32ded7bb8158f04294f0baeb
CECyT9 <CECyT9>
2017-02-23 08:23:39 -0600

    Initial commit @emmanuel789 Práctica escolar de "Hola mundo" para ejemplificar el funcionamiento de GitHub

---
+++ app/src/main/java/cecyt9/ipn/edu/prueba/MainActivity.java
@@ -0,0 +1,16 @@
+package cecyt9.ipn.edu.prueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     * @author Ríos Jiménez Juan Carlos
+     * @author Tejeda Bracho Pável Alejandro
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
